c8e3998bb6a7ee6e9242a6abec0fcd74bf04f0ca
sbx_user1051 <sbx_user1051@169.254.31.94>
2024-12-21 13:03:37 +0000

    Sat Dec 21 13:03:37 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -21,11 +21,32 @@ def checkout(skus):
     # dict{A:1, B: 3...}
 
     # go through all the items
+    total = 0
     for char in items:
         # check if special price
         if char in special_prices.keys():
-            
+            # checks amount of special prices they can get
+            special_price = (letter_counts[char] // special_prices[char]["n"]) * special_prices[char]["p"]
+            # gets left overs from special prices
+            remaining = (letter_counts[char] % special_prices[char]["n"]) * item_prices[char]
 
+            total += (special_price + remaining)
+        else:
+            total += (item_prices[char] * letter_counts[char])
+
+    return total
+
+
+test1 = "AABCDA"
+test2 = "AABCXA"
+test3 = "AAAAA"
+test4 = "AABCD"
+
+if __name__ == "__main__":
+    print(checkout(test1))
+    print(checkout(test2))
+    print(checkout(test3))
+    print(checkout(test4))
 
 
 
