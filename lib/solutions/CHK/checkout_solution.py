c541639c7cec9acc61d92da04e1ff2a2468451a6
sbx_user1051 <sbx_user1051@169.254.31.94>
2024-12-21 13:37:12 +0000

    Sat Dec 21 13:37:12 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,11 +1,23 @@
-
+import numpy as np
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+def discount(counts, special_n, d_price, price):
+    # checks amount of special prices they can get
+    special_price = (counts // special_n) * d_price
+    # gets left overs from special prices
+    remaining = (counts % special_n) * price
+    return special_price + remaining
+
+
 def checkout(skus):
     items = ["A", "B", "C", "D", "E"]
     item_prices = {"A": 50 , "B":30, "C": 20, "D": 15, "E": 40}
-    special_prices = {"A": [{"type": "", "n": 3, "p": 130}, {"n": 5, "p": 200}], "B": {"n": 2, "p": 45}}
+    special_prices = {"A": [{"type": "d", "n": 3, "p": 130}, {"type": "d", "n": 5, "p": 200}],
+                      "B": [{"type": "d", "n": 2, "p": 45}],
+                      "E": [{"type": "f", "n": 2, "i": "B", "q": 1}],
+                      }
 
     letter_counts = {letter: 0 for letter in items}
 
@@ -25,12 +37,25 @@ def checkout(skus):
     for char in items:
         # check if special price
         if char in special_prices.keys():
-            # checks amount of special prices they can get
-            special_price = (letter_counts[char] // special_prices[char]["n"]) * special_prices[char]["p"]
-            # gets left overs from special prices
-            remaining = (letter_counts[char] % special_prices[char]["n"]) * item_prices[char]
+            # checks number of offers
+            if len(special_prices[char]) == 1:
 
-            total += (special_price + remaining)
+                if special_prices[char][0]["type"] == "d":
+                    total += discount(letter_counts[char], special_prices[char][0]["n"], special_prices[char][0]["p"], item_prices[char])
+                elif special_prices[char][0]["type"] == "f":
+                    print("something")
+                else:
+                    print(f"error: {special_prices[char]}")
+            else:
+                n_offers = len(special_prices[char])
+                offers = np.ones(n_offers) * 1000
+
+                for offer in range(n_offers):
+                    # assuming only discount offers for this
+                    offers[offer] = discount(letter_counts[char], special_prices[char][offer]["n"], special_prices[char][offer]["p"], item_prices[char])
+                best_offer = np.min(offers)
+                total += best_offer
+
         else:
             total += (item_prices[char] * letter_counts[char])
 
@@ -42,11 +67,16 @@ test_dic = {
     "Test 2: ": {"t": "BBCX", "r": -1},
     "Test 3: ": {"t": "BBCDA", "r": 130},
     "Test 4: ": {"t": "DCBA", "r": 115},
+    "Test 5: ": {"t": "AAAAAA", "r": 250},
+    "Test 6: ": {"t": "AAAAA", "r": 200},
+    "Test 7: ": {"t": "AAAAAAAAA", "r": 390},
+
 
             }
 if __name__ == "__main__":
     for test in test_dic:
         print(f"{test} {checkout(test_dic[test]['t']) == test_dic[test]['r']}")
+
 
 
 
