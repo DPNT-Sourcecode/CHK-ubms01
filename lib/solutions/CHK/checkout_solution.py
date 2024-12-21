75b7fffada969587cc69baa34403e04e6c198470
sbx_user1051 <sbx_user1051@169.254.31.94>
2024-12-21 12:54:37 +0000

    Sat Dec 21 12:54:37 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,6 +4,29 @@
 # skus = unicode string
 def checkout(skus):
     items = ["A", "B", "C", "D"]
+    item_prices = {"A": 50 , "B":30, "C": 20, "D": 15}
+    special_prices = {"A": {"n": 3, "p": 130}, "B": {"n": 2, "p": 45}}
 
-    
+    letter_counts = {letter: 0 for letter in items}
+
+    # iterate through items
+
+    for char in skus:
+        # if in list
+        if char in items:
+            letter_counts[char] += 1
+        else:
+            return -1
+
+    # dict{A:1, B: 3...}
+
+    # go through all the items
+    for char in items:
+        # check if special price
+        if char in special_prices.keys():
+            
+
+
+
+
 
